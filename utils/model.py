0623277a7abfd211522e322f50cc5f9b84abe58c
Way-Yuhao <liu697@wisc.edu>
2021-11-14 15:58:55 -0600

    added train.py

--- utils/model.py
+++ utils/model.py
@@ -2,13 +2,14 @@
 import torch
 import torch.nn as nn
 
-class RGBCollimator(nn.Module): # TODO
+
+class RGBCollimator(nn.Module):  # TODO
     """Section 3.2 simple lens check"""
     def __init__(self):
-
-
+        super().__init__()
         # trainable height map
-        self.height_map = _height_map_initializer()
+
+        self.height_map = self._height_map_initializer()
 
     def forward(self, x):
 
@@ -17,3 +18,5 @@ class RGBCollimator(nn.Module): # TODO
     def _height_map_initializer(self):
         height_map = torch.full(self.height_map_shape, 1e-4, dtype=torch.float64)
         return nn.parameter.Parameter(height_map, requires_grad=True)
+
+
