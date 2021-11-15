c6dc4d30c4bf733c7dd5f18a704bf491f92e63b7
Krish Kabra <krish@kabra.com>
2021-11-14 18:28:44 -0600

    update optics

--- utils/optics/propagations.py
+++ utils/optics/propagations.py
@@ -6,7 +6,6 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-
 class Propagation(abc.ABC):
     def __init__(self, input_shape, distance, discretization_size, wave_lengths):
         """
@@ -19,47 +18,69 @@ class Propagation(abc.ABC):
         """
         # TODO: what is the structure of input_shape?
         self.input_shape = input_shape
-        self.distance = distance
-        self.wave_lengths = wave_lengths
-        self.wave_nos = 2. * np.pi / wave_lengths  # wavenumber, Eq3
+        self.distance = torch.tensor(distance)
+        self.wave_lengths = torch.tensor(wave_lengths)
+        # self.wave_nos = 2. * np.pi / wave_lengths  # wavenumber, Eq3
         self.discretization_size = discretization_size  # TODO: of sensor?
 
     @abc.abstractmethod
     def _propagate(self, input_field):
-        """ propogates an input field through a medium """
+        """ propogates an input field through a medium
+        """
 
     def __call__(self, input_field):
         return self._propagate(input_field)
 
 
 class FresnelPropagation(Propagation):
+    """
+    Fresnel propogation/diffraction (Equation 3)
+    """
     def _propagate(self, input_field):
-        _, M_orig, N_orig, _ = self.input_shape  # TODO: M=height, N=width?
+        """
+        :param input_field (Tensor[batch_size, height, width, num_wavelengths]): complex valued wavefront
+        """
+
+        _, M_orig, N_orig, _ = self.input_shape
         # zero padding
         Mpad, Npad = M_orig // 4, N_orig // 4
         M = M_orig + 2 * Mpad
         N = N_orig + 2 * Npad
-        # padded_input_field = tf.pad(input_field, [[0, 0], [Mpad, Mpad], [Npad, Npad], [0, 0]])
-        padded_input_field = F.pad(input_field, [0, 0, Mpad, Mpad, Npad, Npad, 0, 0], mode='constant', value=0)
+
+        padded_input_field = F.pad(input_field, [0, 0, Mpad, Mpad, Npad, Npad, 0, 0], mode='constant', value=0.0)
         [x, y] = np.mgrid[-N // 2: N // 2, -M // 2: M // 2]
 
-        # spatial frequency; max frequency = 1 / ( 2 * pixel_size) TODO: why?
-        fx = x / (self.discretization_size * N)
-        fy = y / (self.discretization_size * M)
+        # spatial frequency; max frequency = 1 / ( 2 * pixel_size)
+        fx = torch.tensor(x) / (self.discretization_size * N)
+        fy = torch.tensor(y) / (self.discretization_size * M)
 
         # rearranges a zero-frequency-shifted Fourier transform Y back to the original transform output
         fx = torch.fft.ifftshift(fx)
         fy = torch.fft.ifftshift(fy)
 
-        fx = fx[None, :, :, None]         # TODO: why
+        fx = fx[None, :, :, None]
         fy = fy[None, :, :, None]
 
+        # Transfer function for Fresnel propogation
+        # (see derivation at https://www.cis.rit.edu/class/simg738/Handouts/Derivation_of_Fresnel_Diffraction.pdf)
         squared_sum = torch.square(fx) + torch.square(fy)
+        complex_exponent_part = 2 * np.pi * self.distance * (1/self.wave_lengths) - 1. * np.pi * self.wave_lengths * squared_sum * self.distance
 
-        # create an un-trainable variable so that this computation can be reused from call to call
-        if torch.is_tensor(self.distance):
-            tmp = np.float64(self.wave_lengths * np.pi * -1. * squared_sum)
-            constant_exponent_part = torch.empty(size=padded_input_field.shape, dtype=torch.float64,
-                                                 requires_grad=False)
-            # H = compl_exp_tf() TODO
+        H = torch.exp(torch.complex(torch.zeros_like(complex_exponent_part), complex_exponent_part))
 
+        objFT = torch.fft.fft2(padded_input_field, dim=(1,2))
+        # Convolution
+        out_field = torch.fft.ifft2(objFT * H, dim=(1,2))
+
+        return out_field
+
+def propagate_fresnel(input_field,
+                      distance,
+                      sampling_interval,
+                      wave_lengths):
+    input_shape = input_field.shape.as_list()
+    propagation = FresnelPropagation(input_shape,
+                                     distance=distance,
+                                     discretization_size=sampling_interval,
+                                     wave_lengths=wave_lengths)
+    return propagation(input_field)
